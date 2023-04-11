63c4fdfb0fee73e3af2616b11de732bfaaf106fb
Riley S <Github@rileystrickland.com>
2023-04-10 23:55:00 -0400

    Create xcl.h

---
+++ xcl.h
@@ -0,0 +1,18 @@
+#pragma once
+namespace xcl
+{
+	template<typename T>
+	concept integral = std::is_integral_v<T>;
+
+	template<typename T>
+	concept enumerable = std::is_enum_v<T>;
+
+	template<typename T1, typename T2>
+	concept different_from = !std::is_same_v<T1, T2>;
+
+	template<typename T>
+	struct has_flag
+	{
+
+	};
+}
